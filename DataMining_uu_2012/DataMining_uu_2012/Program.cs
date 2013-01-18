12f8fd367a5e94c668471202dc6448fcb81ee90c
Mike Roylance <roylance.michael@gmail.com>
2013-01-17 23:05:46 -0700

    simple program, average right now for the BirthdayParadox for n=1000 is about 50~

---
+++ DataMining_uu_2012/DataMining_uu_2012/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataMining_uu_2012
+{
+	using DataMining_uu_2012.hw1;
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var bdp = new BirthdayParadox();
+			//let's average it out a hundred times
+			var nums = new List<int>();
+
+			for (var i = 0; i < 100; i++)
+			{
+				var num = bdp.GenerateRandomNumbers(1000); 
+				nums.Add(num);
+				Console.WriteLine(num);	
+			}
+			Console.WriteLine("Average amount was: " + nums.Sum()+ "/" + nums.Count);
+			Console.ReadKey();
+		}
+	}
+}
---
+++ DataMining_uu_2012/DataMining_uu_2012/hw1/BirthdayParadox.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataMining_uu_2012.hw1
+{
+	public class BirthdayParadox
+	{
+		/// <summary>
+		/// generating random numbers of n until we have a duplicate, returning how many it takes to get it
+		/// </summary>
+		/// <param name="n"></param>
+		/// <returns></returns>
+		public int GenerateRandomNumbers(int n)
+		{
+			var k = 0;
+			var awesomeHash = new HashSet<int>();
+			var randomGen = new Random();
+			var foundDup = false;
+			while (!foundDup)
+			{
+				var num = randomGen.Next(1, n);
+				if (awesomeHash.Contains(num))
+				{
+					foundDup = true;
+				}
+				else
+				{
+					awesomeHash.Add(num);
+					k++;
+				}
+			}
+			return k;
+		}
+	}
+}
